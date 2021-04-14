96a940a4b13f720fc1954f2bed4417a1ff1b46e0
ross.mcdermott <ross.mcdermott@aboutyou.com>
2021-04-14 10:08:24 +0200

    Tidying up.

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -2,12 +2,10 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
-// import { environment }
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {
-    // console.log(environment);
   }
 
   @Get('hello')
