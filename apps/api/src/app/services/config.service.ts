392a9b196a58e15c7362753fbb248bcb9ff2729e
ross.mcdermott <ross.mcdermott@aboutyou.com>
2021-04-15 12:12:52 +0200

    addind env and config

---
+++ apps/api/src/app/services/config.service.ts
@@ -0,0 +1,13 @@
+import { HttpService, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ConfigService {
+
+  constructor() {}
+
+  getConfig() {
+
+  }
+
+}
